4310ffa039d6f2d0fea0e2f9776215cd0a7eb2a4
Kyle Drake <blazehavenservers@gmail.com>
2023-04-08 13:47:12 -0700

    using more tk stuff

--- main.py
+++ main.py
@@ -1,56 +1,93 @@
 import os
-import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
 
-def browse_dir():
-    global selected_dir
-    selected_dir = filedialog.askdirectory()
-    if selected_dir:
-        dir_label.config(text=selected_dir)
 
 def rename_files():
-    if not selected_dir:
-        messagebox.showwarning("Warning", "Please select a directory")
-        return
     old_ext = old_ext_entry.get().strip()
     new_ext = new_ext_entry.get().strip()
-    if not old_ext or not new_ext:
-        messagebox.showwarning("Warning", "Please enter old and new extensions")
+
+    if add_num_checkbox_var.get():
+        try:
+            num = int(starting_num_entry.get().strip())
+        except ValueError:
+            messagebox.showerror("Error", "Please enter a valid starting number.")
+            return
+    else:
+        num = None
+
+    if not os.path.exists(directory_path.get()):
+        messagebox.showerror("Error", "Invalid directory path.")
         return
-    if old_ext[0] != '.':
-        old_ext = '.' + old_ext
-    if new_ext[0] != '.':
-        new_ext = '.' + new_ext
-    for filename in os.listdir(selected_dir):
-        if filename.endswith(old_ext):
-            base_name = os.path.splitext(filename)[0]
-            new_name = base_name + new_ext
-            os.rename(os.path.join(selected_dir, filename), os.path.join(selected_dir, new_name))
-    messagebox.showinfo("Success", "File extensions have been changed")
 
-window = tk.Tk()
-window.geometry("600x300")
-window.title("File Extension Changer")
+    count = 0
 
-browse_button = tk.Button(window, text="Select Directory", command=browse_dir)
-browse_button.pack(pady=10)
+    for file in os.listdir(directory_path.get()):
+        if file.endswith(old_ext):
+            filename, ext = os.path.splitext(file)
+            new_filename = filename + "_" + str(num) if num else filename
+            new_filename += "." + new_ext if new_ext else ext
 
-dir_label = tk.Label(window, text="No directory selected")
-dir_label.pack()
+            os.rename(
+                os.path.join(directory_path.get(), file),
+                os.path.join(directory_path.get(), new_filename)
+            )
+            count += 1
+            num += 1 if num is not None else 0
 
-old_ext_label = tk.Label(window, text="Enter old extension:")
-old_ext_label.pack(pady=10)
+    messagebox.showinfo("Success", f"Successfully renamed {count} files.")
 
-old_ext_entry = tk.Entry(window)
-old_ext_entry.pack()
 
-new_ext_label = tk.Label(window, text="Enter new extension:")
-new_ext_label.pack(pady=10)
+def select_directory():
+    dir_path = filedialog.askdirectory()
+    directory_path.set(dir_path)
 
-new_ext_entry = tk.Entry(window)
-new_ext_entry.pack()
 
-run_button = tk.Button(window, text="Run", command=rename_files)
-run_button.pack(pady=10)
+def toggle_num_textbox():
+    starting_num_entry.config(state=NORMAL if add_num_checkbox_var.get() else DISABLED)
 
+
+# Create the main window
+window = Tk()
+window.geometry("800x600")
+window.title("File Renamer")
+
+# Set up variables
+directory_path = StringVar()
+add_num_checkbox_var = BooleanVar()
+
+# Add widgets to the window
+select_dir_button = Button(window, text="Select Directory", command=select_directory)
+select_dir_button.grid(column=0, row=0, padx=10, pady=20)
+
+directory_entry = Entry(window, textvariable=directory_path, width=50)
+directory_entry.grid(column=1, row=0, padx=10, pady=20)
+
+old_ext_label = Label(window, text="Enter current extension:")
+old_ext_label.grid(column=0, row=1)
+
+old_ext_entry = Entry(window)
+old_ext_entry.grid(column=1, row=1)
+
+new_ext_label = Label(window, text="Enter new extension:")
+new_ext_label.grid(column=0, row=2)
+
+new_ext_entry = Entry(window)
+new_ext_entry.grid(column=1, row=2)
+
+add_num_checkbox = Checkbutton(window, text="Add number to filename", variable=add_num_checkbox_var,
+                               command=toggle_num_textbox)
+add_num_checkbox.grid(column=0, row=3)
+
+starting_num_label = Label(window, text="Enter Number if applicable:")
+starting_num_label.grid(column=0, row=4)
+
+starting_num_entry = Entry(window, state=DISABLED)
+starting_num_entry.grid(column=1, row=4)
+
+run_button = Button(window, text="Run", command=rename_files)
+run_button.grid(column=1, row=5, padx=10, pady=20)
+
+# Run the main loop
 window.mainloop()
